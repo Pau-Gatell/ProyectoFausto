a24df2c375901464827fc8e8fc3dae6a08effa10
Pau-Gatell <455837.clot@fje.edu>
2026-05-04 12:34:44 +0200

    Add MainMenu and PauseMenu UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void OpenNorthpoint()
+    {
+        Application.OpenURL("https://northpointwear.com/"); // cambia por la web real
+    }
+
+
+    // ▶️ Cargar escena del juego
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene"); // nombre EXACTO de tu escena
+    }
+
+    // ❌ Salir del juego
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Salir del juego"); // para probar en Unity
+    }
+}
